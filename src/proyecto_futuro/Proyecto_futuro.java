c2578b39622929b41fc5085af20074601190f3bd
linuxmint20 <linuxmint20@linuxmint20-virtual-machine>
2020-11-15 13:33:34 +0100

    Veredicto public a privado.

--- src/proyecto_futuro/Proyecto_futuro.java
+++ src/proyecto_futuro/Proyecto_futuro.java
@@ -31,7 +31,7 @@ public class Proyecto_futuro {
 
     }
 
-    static String veredicto(String valor1, String valor2) {
+    private static String veredicto(String valor1, String valor2) {
 
         if (valor1.equals(valor2)) {
             return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
